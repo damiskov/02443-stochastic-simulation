0e8e25f224a886739617c3bd1a31f159fe88918d
David Miles-Skov <davidmilesskov@protonmail.com>
2023-06-06 23:42:19 +0200

    Update ex3.py

--- Exercises/ex3.py
+++ Exercises/ex3.py
@@ -22,10 +22,53 @@ def pareto(k, beta, U):
 def box_muller(U1, U2):
     return np.sqrt(-2*np.log(U1))*np.cos(2*np.pi*U2), np.sqrt(-2*np.log(U1))*np.sin(2*np.pi*U2)
 
+def hyper_exp(probabilities, lambdas):
+    
+    # Using "crude" method to choose I
+    
+    cdf = np.cumsum(probabilities)
+    intervals = [(0, cdf[0])]+[(cdf[i-1], cdf[i]) for i in range(1, len(cdf))]
+    i_continuous = np.random.random(low=0, high=1)
+    i_discrete = 0
+
+    for i, interval in enumerate(intervals):
+        if i_continuous > interval[0] and i_continuous <= interval[1]:
+
+            i_discrete = i
+
+    Z = exponential(lambdas[i_discrete], np.random.random(size=10000))
+    return Z
+
+
+def erlang(l, n):
+    U = np.random.random(low=1, high=10, size=n)
+    prod = 1
+    for i in range(n):
+        prod *= U[i]
+    return (-1/l)*log(prod)
+
+
+
 
 
 def main():
-    
+
+    # 1 - Generating distributions and testing/comparing
+
+    # a) Exponential
+
+    U = np.random.random(size=10000)
+    l = 0.2
+    exp_dist = exponential(l, U)
+
+    # b) Normal
+
+    # c) Pareto
+    # i) k = 2.05
+    # ii) k = 2.5
+    # iii) k = 3
+    # iv) k = 4
+
     return
 
 if __name__=="__main()__":
